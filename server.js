6ce9efbf5669a2a20c4f2e6662e04504dc1d6561
Marcelo Pata <56968177+patamarcelo@users.noreply.github.com>
2022-07-14 11:02:01 -0300

    adjust serve

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const serveStatic = require("serve-static");
 
 const app = express();
 
-app.use("/", serveStatic(path.join(__dirname, "/dist")));
+app.use("/", serveStatic(path.join(__dirname, "/dist", 'index.html')));
 
 const port = process.env.PORT || 8080;
 app.listen(port);
